71a3b473dbf8055f12523bd3789444a0f014eed7
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2022-12-05 09:47:02 -0600

    added logic for types, working on comparing objects

--- js/script.js
+++ js/script.js
@@ -7,12 +7,18 @@
 const $input = $('input[type="text"]');
 const $pokemon = $('#pokemon');
 const $moveSet = $('#moveSet');
-const $type = $("#type");
+const $type = $('#type');
+const $secondPlayerType = $('#player-type2')
+const $enemyPokemon = $('#enemy-pokemon');
+const $enemyMoveSet = $('#enemy-moveset');
+const $enemyType = $('#enemy-type');
+const $enemyInput = $('input[type="text-box"]'); // had to name the enemy input something different for it to execute
+const $secondEnemyType = $('#enemy-type2');
 const nationalDex = `https://pokeapi.co/api/v2/pokemon/`; // i think to only the top 20 pokemon for some reason
 // /limit2000&offset=0
 // ${($input)}
 
-//not sure how to input the name from a text box but it holds 
+//not sure how to input the name from a  text box but it holds 
 
 /////////
 // ////juqery elements/////
@@ -20,25 +26,25 @@ const nationalDex = `https://pokeapi.co/api/v2/pokemon/`; // i think to only the
 
 $body = $("#body");
 $pokeSubmit = $("#pokeSubmit");
-$form = $("#form-section");
+$form = $("#form-section-player");
+$enemyForm = $("#form-section-enemy");
 
 /////
 // addEventListener/////
 ///////////////////
 
-  $form.on("submit", getNationalDex); // listeening for thhe button to action. also works with the enter button 
-
+$form.on("submit", getNationalDex); // listeening for thhe button to action. also works with the enter button 
+$enemyForm.on("submit", getEnemyNationalDex);
 
 // ////////
 // functions
 // ////////
 
-//getNationalDex();
+//getNationalDex for the player 
 //you can replace console.log with render(data.results) here to pushh the info to the next function console.log(data)
 function getNationalDex(event) {
   event.preventDefault(); //allows mee to stop the submit button from functioning  for example
   userInput = $input.val(); //get value from the input on the textbox
-  userInput.charAt(0).toUpperCase; 
   if (userInput === "") return;
   $input.val("");//starts with a blank input i believe 
   $.ajax(nationalDex + userInput) //gets url and adds the userinput
@@ -57,21 +63,71 @@ function getNationalDex(event) {
         })
           .text(move.move.name)
           .appendTo($moveSet);
-      }
-      
+      }    
       $type.text(data.types[0].type.name);
+      $secondPlayerType.text(data.types[1].type.name);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  
+  //   function render(pokemon) {
+  //     //
+}
 
 
 
 
+//////////////////////////////////////
+///ENEMY GET NATIONAL DEX FUNCTION/////
+
+
+function getEnemyNationalDex(event) {
+  event.preventDefault(); //allows mee to stop the submit button from functioning  for example
+  userInput = $enemyInput.val(); //get value from the input on the textbox
+  // userInput.charAt(0).toUpperCase;
+  if (userInput === "") return;
+  $enemyInput.val(""); //starts with a blank input i believe
+  $.ajax(nationalDex + userInput) //gets url and adds the userinput
+    .then((data) => {
+      console.log(data); //    render(data.results)
+      $enemyPokemon.text(data.name);
+      // $moveSet.text(data.moves[0]);
+      for (const move of data.moves) {
+        //iterates over moveset array in api
+        $("<p>", {
+          // this creates a new <p> for every move in the array
+          id: "move",
+          class: "moves",
+        })
+          .text(move.move.name)
+          .appendTo($enemyMoveSet);
+      }
+      $enemyType.text(data.types[0].type.name);
+      $secondEnemyType.text(data.types[1].type.name);
+
+      if ($secondEnemyType == "") {
+         $("<p>", {
+           text: "no second typing",// this creates a new <p> for every move in the array
+           id: "move",
+           class: "moves",
+         });
+      }
 
     })
     .catch(function (error) {
       console.log(error);
     });
-  //   function render(pokemon) {
-  //     //
 }
+
+
+
+///////////////////////
+// objects 
+  //////////
+
+
+ 
 
 /////psudocode
 /*
